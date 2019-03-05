bf7860aba98ca4881641f6a4a897e11a487ccaef
Simo Rinne <rinne.simo@gmail.com>
2019-03-05 21:21:46 +0200

    Added RandomSpawn script

---
+++ GoblinKing/Assets/GoblinKing/Scripts/Helpers/RandomSpawn.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GoblinKing.Helpers
+{
+    public class RandomSpawn : MonoBehaviour
+    {
+        public int SpawnChance = 50;
+
+        void Awake()
+        {
+            if (Random.Range(0, 100) >= SpawnChance)
+            {
+                GameObject.Destroy(gameObject);
+            }
+        }
+    }
+}
